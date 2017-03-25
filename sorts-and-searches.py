bf355dd6bce0e9e5ab5e0bba566cd5fb28ecf9b0
Chris Shoemaker <shoemakerc@carleton.edu>
2017-03-24 18:12:46 -0700

    Making a better name for this program

--- sorts-and-searches.py
+++ sorts-and-searches.py
@@ -1,3 +1,5 @@
+from random import shuffle
+
 ## BINARY SEARCH ##
 def binarySearchHelper(arr, target, lo, hi):
 	if lo > hi:
@@ -9,6 +11,7 @@ def binarySearchHelper(arr, target, lo, hi):
 		return binarySearchHelper(arr, target, mid + 1, hi)
 	else:
 		return binarySearchHelper(arr, target, 0, mid - 1)
+
 ## INSERTION SORT ##
 def insertionSort(arr):
 	for i in range(1, len(arr)):
@@ -20,6 +23,7 @@ def insertionSort(arr):
 		arr[j + 1] = x
 	return arr
 
+## SELECTION SORT (w/ swap) ##
 def selectionSort(arr):
 	for i in range(len(arr)):
 		min = i
@@ -28,22 +32,29 @@ def selectionSort(arr):
 				min = j
 		swap(arr, min, i)
 	return arr
-
 def swap(arr, x, y):
 	temp = arr[x]
 	arr[x] = arr[y]
 	arr[y] = temp
 
+'''
+## BEST SORT ##
+def bogoSort(arr):
+	while sorted(arr) != arr:
+		shuffle(arr)
+		print("Bogo!", arr)
+	return arr
+'''
+
 def binarySearch(arr, target):
 	return binarySearchHelper(arr, target, 0, len(arr) - 1)
 
 def main():
-	unsortedArr = [2, 5, 2, 4, 12, 5, 1, 5, 8, 9, 44, 2, 1243, 6, 6, 3, 4]
 	searchArr = [1, 3, 5, 7, 9]
-	print("Unsorted array is:", unsortedArr)
-	print("Unsorted array after insertion sort:", insertionSort(unsortedArr))
-	print("Unsorted array after selection sort:", selectionSort(unsortedArr))
-	# print(selectionSort(unsortedArr))
+	print("Unsorted array is:", [2, 5, 2, 4, 12, 5, 1, 5, 8, 9, 44, 2, 1243, 6, 6, 3, 4])
+	print("Unsorted array after insertion sort:", insertionSort([2, 5, 2, 4, 12, 5, 1, 5, 8, 9, 44, 2, 1243, 6, 6, 3, 4])) # these long inputs...damn you, side effects!
+	print("Unsorted array after selection sort:", selectionSort([2, 5, 2, 4, 12, 5, 1, 5, 8, 9, 44, 2, 1243, 6, 6, 3, 4]))
+	#print("Unsorted array after bogosort:", bogoSort([2, 5, 2, 4, 12, 5, 1, 5, 8, 9, 44, 2, 1243, 6, 6, 3, 4]))
 	index = binarySearch(searchArr, 8)
 	if index < 0:
 		print("Error: Element not found at any index")
