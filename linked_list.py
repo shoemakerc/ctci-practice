c185be953b3329cce279d44bca66ea53ea5759f8
Chris Shoemaker <shoemakerc@carleton.edu>
2017-08-13 23:35:04 -0700

    fixed llist duplicate delete

--- linked_list.py
+++ linked_list.py
@@ -17,15 +17,11 @@ class LinkedList:
     def deleteDups(self):
         if not self.head:
             return
-
         d = {}
-        n = self.head
-        d[n.data] = 1
-        n = n.next
         prev = Node()
+        n = self.head
         while n:
             if n.data in d:
-                print("Removing", n.data)
                 prev.next = n.next
             else:
                 d[n.data] = 1
@@ -52,25 +48,22 @@ class Node:
         self.next = newNext
 
 def main():
-    llist = LinkedList(Node(2))
+    llist = LinkedList(Node(1))
+    llist.appendToTail(1)
+    llist.appendToTail(2)
+    llist.appendToTail(2)
     llist.appendToTail(2)
     llist.appendToTail(3)
+    llist.appendToTail(3)
+    llist.appendToTail(3)
+    llist.appendToTail(3)
     llist.appendToTail(4)
-    llist.appendToTail(1)
-    llist.appendToTail(6)
-    llist.appendToTail(2)
-    llist.appendToTail(1)
-    llist.appendToTail(9)
-    llist.appendToTail(1)
-    llist.appendToTail(9)
-    llist.appendToTail(9)
-    llist.appendToTail(9)
-    llist.appendToTail(9)
+    llist.appendToTail(4)
+    llist.appendToTail(4)
+    llist.appendToTail(4)
+    llist.appendToTail(5)
     llist.printNodes()
     print("")
     llist.deleteDups()
-    ar = []
-    while llist:
-        ar.append(llist.head.getData())
-        llist.head = llist.head.next
+    llist.printNodes()
 main()
